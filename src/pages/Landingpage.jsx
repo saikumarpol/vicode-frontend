6618ef09f37f4dda153f28539279d4ada0aebfd2
saikumarpola <92100989+saikumarpol@users.noreply.github.com>
2025-03-26 17:41:19 +0530

    Update Landingpage.jsx

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { FaCode, FaRocket, FaLaptopCode } from "react-icons/fa";
 import { useNavigate } from "react-router-dom"; 
-import "./LandingPage.css";
+import './Landingpage.css'
 
 export default function LandingPage() {
     const navigate = useNavigate();
