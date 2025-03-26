ce86a246e96c753d4b66411140921acba51c59cc
saikumarpola <92100989+saikumarpol@users.noreply.github.com>
2025-03-26 17:38:29 +0530

    Update Landingpage.jsx

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { FaCode, FaRocket, FaLaptopCode } from "react-icons/fa";
 import { useNavigate } from "react-router-dom"; 
-import "./LandingPage.css";  // Added external CSS for Bianka font
+import "./LandingPage.css";
 
 export default function LandingPage() {
     const navigate = useNavigate();
